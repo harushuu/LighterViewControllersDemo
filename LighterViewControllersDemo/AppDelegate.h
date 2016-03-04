ff4126c719126d4a30e325858f8cd62e84ddf76c
@harushuu <hunter4n@gmail.com>
2016-03-04 10:29:58 +0800

    Initial Commit

---
+++ LighterViewControllersDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LighterViewControllersDemo
+//
+//  Created by shuu on 16/3/4.
+//  Copyright © 2016年 shuu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
